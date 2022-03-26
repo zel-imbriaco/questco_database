45cfa95036529109536da29eade3e8efd313ae51
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-26 11:08:31 -0400

    Improve User Story 3 spec, create User Story 4 spec.

--- spec/features/adventurers/adventurers_spec.rb
+++ spec/features/adventurers/adventurers_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe '/adventurers endpoint', type: :feature do
 
   it 'User Story 3' do
     party_1 = Party.create(name: "The Mighty Nein", rank: 1, active: true)
+    party_2 = Party.create(name: "Bell's Helles", rank: 2, active: false)
     adventurer_1 = Adventurer.create(name: "Beauregard Lionett", level: 9, alive: true, party_id: party_1.id)
     adventurer_2 = Adventurer.create(name: "Mollymauk Tealeaf", level: 5, alive: false, party_id: party_1.id)
 
@@ -16,5 +17,20 @@ RSpec.describe '/adventurers endpoint', type: :feature do
     expect(page).to have_content(adventurer_2.name)
     expect(page).to have_content(adventurer_2.level)
     expect(page).to have_content(adventurer_2.alive)
+    expect(page).to have_no_content(party_2.name)
+  end
+
+  it 'User Story 4' do
+    party_1 = Party.create(name: "The Mighty Nein", rank: 1, active: true)
+    party_2 = Party.create(name: "Bell's Helles", rank: 2, active: false)
+    adventurer_1 = Adventurer.create(name: "Beauregard Lionett", level: 9, alive: true, party_id: party_1.id)
+    adventurer_2 = Adventurer.create(name: "Mollymauk Tealeaf", level: 5, alive: false, party_id: party_1.id)
+    # When I visit '/child_table_name/:id'
+    visit "/adventurers/#{adventurer_2.id}"
+    # Then I see the child with that id including the child's attributes:
+    expect(page).to have_content(adventurer_2.name)
+    expect(page).to have_content(adventurer_2.level)
+    expect(page).to have_content(adventurer_2.alive)
+    expect(page).to have_content(party_1.name)
   end
 end
