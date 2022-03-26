e9165d11cbc1dc0d65efafc7ee1fca91ebeeba23
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-26 09:52:06 -0400

    Create User Story 2 feature test.

--- spec/features/parties/parties_spec.rb
+++ spec/features/parties/parties_spec.rb
@@ -2,12 +2,25 @@ require 'rails_helper'
 
 RSpec.describe '/parties endpoint', type: :feature do
 
-  it 'Returns all party names' do
-    party_1 = Party.create(name: "The Mighty Nein", rank: 1, active: true)
+  it 'User Story 1' do
+    party_1 = Party.create(name: "The Mighty Nein", rank: 2, active: true)
+    party_2 = Party.create(name: "Vox Machina", rank: 1, active: false)
 
     # When I visit '/parents'
     visit '/parties'
     # Then I see the name of each parent record in the system
     expect(page).to have_content("The Mighty Nein")
+  end
+
+  it 'User Story 2' do
+    party_1 = Party.create(name: "The Mighty Nein", rank: 2, active: true)
+    party_2 = Party.create(name: "Vox Machina", rank: 1, active: false)
+    # When I visit '/parents/:id'
+    visit '/parties/1'
+    # Then I see the parent with that id including the parent's attributes:
+    # - data from each column that is on the parent table
+    expect(page).to have_content(party_1.name)
+    expect(page).to have_content(party_1.rank)
+    expect(page).to have_content(party_1.active)
   end
 end
