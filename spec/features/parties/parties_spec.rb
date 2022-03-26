56e529d6b7d5d9fa7d7c5b351a8cfb45d41acc23
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-26 11:20:33 -0400

    Create User Story 5 spec.

--- spec/features/parties/parties_spec.rb
+++ spec/features/parties/parties_spec.rb
@@ -25,4 +25,20 @@ RSpec.describe '/parties endpoint', type: :feature do
     expect(page).to have_content(party_1.rank)
     expect(page).to have_content(party_1.active)
   end
+
+  it 'User Story 5' do
+    party_1 = Party.create(name: "The Mighty Nein", rank: 1, active: true)
+    party_2 = Party.create(name: "Bell's Helles", rank: 2, active: false)
+    adventurer_1 = Adventurer.create(name: "Beauregard Lionett", level: 9, alive: true, party_id: party_1.id)
+    adventurer_2 = Adventurer.create(name: "Mollymauk Tealeaf", level: 5, alive: false, party_id: party_1.id)
+    # When I visit '/parents/:parent_id/child_table_name'
+    visit "/parties/#{party_1.id}/adventurers"
+    # Then I see each Child that is associated with that Parent with each Child's attributes:
+    expect(page).to have_content(adventurer_1.name)
+    expect(page).to have_content(adventurer_1.level)
+    expect(page).to have_content(adventurer_1.alive)
+    expect(page).to have_content(adventurer_2.name)
+    expect(page).to have_content(adventurer_2.level)
+    expect(page).to have_content(adventurer_2.alive)
+  end
 end
