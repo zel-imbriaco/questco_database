0075269f45f0f88dc5a29f577d12f2a9a102d74d
Zachary Imbriaco <zachary@imbriaco.com>
2022-03-28 15:42:10 -0400

    Create User Story 12 feature test.

--- spec/features/parties/parties_spec.rb
+++ spec/features/parties/parties_spec.rb
@@ -107,4 +107,24 @@ RSpec.describe '/parties endpoint', type: :feature do
     expect(current_path).to eq("/parties")
     expect(page).to have_content("The Answer")
   end
+
+  it 'User Story 12' do
+    party_1 = Party.create(name: "The Answer", rank: 4, active: true)
+    # When I visit a parent show page
+    visit "/parties/#{party_1.id}"
+    # Then I see a link to update the parent "Update Parent"
+    # When I click the link "Update Parent"
+    click_link "Update Parent"
+    # Then I am taken to '/parents/:id/edit' where I  see a form to edit the parent's attributes:
+    expect(current_path).to eq("/parties/#{party_1.id}/edit")
+    # When I fill out the form with updated information
+    fill_in 'Name', with: "The Kings' Guard"
+    # And I click the button to submit the form
+    click_on 'Update Party'
+    # Then a `PATCH` request is sent to '/parents/:id',
+    # the parent's info is updated,
+    # and I am redirected to the Parent's Show page where I see the parent's updated info
+    expect(current_path).to eq("/parties/#{party_1.id}")
+    expect(page).to have_content("The Kings' Guard")
+  end
 end
